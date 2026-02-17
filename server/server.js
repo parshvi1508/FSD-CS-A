93545fa2d471c4852dbfd4725434920a74c6d3d2
Parshvi Jain <parshvijain1508@gmail.com>
2026-02-17 14:13:34 +0530

    add server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import http from "http";
 import os from "os";
-const port = 4000;
+const port = 4001;
 let body = "";
+const data = [];
 const server = http.createServer((req, res) => {
   const url = req.url;
   if (url == "/" && req.method == "GET") {
@@ -11,24 +12,31 @@ const server = http.createServer((req, res) => {
   } else if (url == "/contact" && req.method == "GET") {
     res.end("Contact Page ");
   } else if (url == "/system" && req.method == "GET") {
-    res.end("System info");
+    const sysdata = {
+      platform: os.platform(),
+      arch: os.arch(),
+      cpu: os.cpus().length,
+      totalmem: (os.totalmem() / 1024 ** 3).toFixed(2)+"GB",
+      freemem: (os.freemem() / 1024 ** 3).toFixed(2)+"GB",
+    };
+    res.setHeader("content-Type", "application/json");
+    res.end(JSON.stringify(sysdata));
   } else if (url == "/senddata" && req.method == "POST") {
-    
     req.on("data", (chunk) => {
       body = body + chunk;
     });
     req.on("end", () => {
       res.statusCode = 201;
       console.log(body, "datasend");
-      res.end(body + "<h1>Send data</h1>");
+      data.push(body);
+      res.end(JSON.stringify(data));
     });
   } else if (url == "/viewdata" && req.method == "GET") {
-    res.end("view data");
+    res.end(body);
   } else {
-    res.statuscode = 404;
+    res.statusCode = 404;
     res.end("Error Page");
   }
-  res.end();
 });
 server.listen(port, () => {
   console.log(`Server is running on ${port}`);
